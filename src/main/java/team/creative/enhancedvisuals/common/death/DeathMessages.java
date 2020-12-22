384936b0dd75da0ad0218747db412942f03503a2
CreativeMD <Creative-Multimedia-Design@gmx.de>
2020-12-22 16:01:12 +0100

    Fixed using deathmessage array instead of list

--- src/main/java/team/creative/enhancedvisuals/common/death/DeathMessages.java
+++ src/main/java/team/creative/enhancedvisuals/common/death/DeathMessages.java
@@ -1,5 +1,7 @@
 package team.creative.enhancedvisuals.common.death;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import team.creative.creativecore.common.config.api.CreativeConfig;
@@ -8,16 +10,16 @@ import team.creative.creativecore.common.config.api.ICreativeConfig;
 public class DeathMessages implements ICreativeConfig {
 	
 	@CreativeConfig
-	public String[] deathMessages = new String[] { "Do you really want to respawn? think of it again.", "Life is hard. Deal with it!", "You are dead ... wait you already know that.", "Did I let the stove on...?", "Should have shot back first...", "Yep, that's messed up...", "Rage incomming!", "I think you dropped something.", "Time for a break?" };
+	public List<String> deathMessages = Arrays.asList("Do you really want to respawn? think of it again.", "Life is hard. Deal with it!", "You are dead ... wait you already know that.", "Did I let the stove on...?", "Should have shot back first...", "Yep, that's messed up...", "Rage incomming!", "I think you dropped something.", "Time for a break?");
 	
 	public boolean enabled = true;
 	
 	private Random rand = new Random();
 	
 	public String pickRandomDeathMessage() {
-		if (deathMessages.length == 0)
+		if (deathMessages.size() == 0)
 			return null;
-		return deathMessages[rand.nextInt(deathMessages.length - 1)];
+		return deathMessages.get(rand.nextInt(deathMessages.size() - 1));
 	}
 	
 	@Override
