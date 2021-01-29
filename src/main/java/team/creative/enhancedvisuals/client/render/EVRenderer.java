dbb7f143029c76bad3168f6afc05c8c6c6f1fb44
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-01-29 14:35:17 +0100

    Fixed not displaying different deathmessages

--- src/main/java/team/creative/enhancedvisuals/client/render/EVRenderer.java
+++ src/main/java/team/creative/enhancedvisuals/client/render/EVRenderer.java
@@ -19,98 +19,98 @@ import team.creative.enhancedvisuals.client.EVClient;
 import team.creative.enhancedvisuals.client.VisualManager;
 
 public class EVRenderer {
-	
-	private static Minecraft mc = Minecraft.getMinecraft();
-	
-	private static String lastRenderedMessage;
-	
-	private static int framebufferWidth;
-	private static int framebufferHeight;
-	
-	@SubscribeEvent
-	public static void render(RenderTickEvent event) {
-		if (event.phase == Phase.END && EVClient.shouldRender()) {
-			
-			if (!(mc.currentScreen instanceof GuiGameOver)) {
-				if (mc.getFramebuffer().framebufferWidth != framebufferWidth || mc.getFramebuffer().framebufferHeight != framebufferHeight) {
-					for (VisualType type : VisualType.getTypes())
-						type.resize(mc.getFramebuffer());
-					framebufferWidth = mc.getFramebuffer().framebufferWidth;
-					framebufferHeight = mc.getFramebuffer().framebufferHeight;
-				}
-				
-				ScaledResolution resolution = new ScaledResolution(mc);
-				int screenWidth = resolution.getScaledWidth();
-				int screenHeight = resolution.getScaledHeight();
-				
-				GlStateManager.pushMatrix();
-				TextureManager manager = mc.getTextureManager();
-				float partialTicks = event.renderTickTime;
-				
-				RenderHelper.enableStandardItemLighting();
-				GlStateManager.disableLighting();
-				GlStateManager.clear(256);
-				GlStateManager.matrixMode(5889);
-				GlStateManager.loadIdentity();
-				GlStateManager.ortho(0.0D, screenWidth, screenHeight, 0.0D, 1000.0D, 3000.0D);
-				GlStateManager.matrixMode(5888);
-				GlStateManager.loadIdentity();
-				GlStateManager.translate(0.0F, 0.0F, -2000.0F);
-				
-				GlStateManager.disableBlend();
-				GlStateManager.disableDepth();
-				GlStateManager.depthMask(false);
-				GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-				GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-				GlStateManager.disableAlpha();
-				GlStateManager.enableBlend();
-				
-				renderVisuals(VisualManager.visuals(VisualCategory.overlay), manager, screenWidth, screenHeight, partialTicks);
-				renderVisuals(VisualManager.visuals(VisualCategory.particle), manager, screenWidth, screenHeight, partialTicks);
-				
-				GlStateManager.matrixMode(5890);
-				GlStateManager.pushMatrix();
-				GlStateManager.loadIdentity();
-				renderVisuals(VisualManager.visuals(VisualCategory.shader), manager, screenWidth, screenHeight, partialTicks);
-				GlStateManager.popMatrix();
-				
-				GlStateManager.depthMask(true);
-				GlStateManager.enableDepth();
-				GlStateManager.enableAlpha();
-				GlStateManager.disableLighting();
-				
-				mc.getFramebuffer().bindFramebuffer(false);
-				GlStateManager.matrixMode(5888);
-				
-				GlStateManager.popMatrix();
-				
-			} else {
-				if (EnhancedVisuals.MESSAGES.enabled) {
-					if (lastRenderedMessage == null)
-						lastRenderedMessage = EnhancedVisuals.MESSAGES.pickRandomDeathMessage();
-					
-					if (lastRenderedMessage != null)
-						mc.fontRenderer.drawString("\"" + lastRenderedMessage + "\"", mc.currentScreen.width / 2 - mc.fontRenderer.getStringWidth(lastRenderedMessage) / 2, 114, 16777215);
-				}
-			}
-		}
-	}
-	
-	private static void renderVisuals(Collection<Visual> visuals, TextureManager manager, int screenWidth, int screenHeight, float partialTicks) {
-		if (visuals == null || visuals.isEmpty())
-			return;
-		try {
-			
-			for (Visual visual : visuals) {
-				if (visual.isVisible()) {
-					GlStateManager.pushMatrix();
-					visual.render(manager, screenWidth, screenHeight, partialTicks);
-					GlStateManager.popMatrix();
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+    
+    private static Minecraft mc = Minecraft.getMinecraft();
+    
+    private static String lastRenderedMessage;
+    
+    private static int framebufferWidth;
+    private static int framebufferHeight;
+    
+    @SubscribeEvent
+    public static void render(RenderTickEvent event) {
+        if (event.phase == Phase.END && EVClient.shouldRender()) {
+            
+            if (!(mc.currentScreen instanceof GuiGameOver)) {
+                if (mc.getFramebuffer().framebufferWidth != framebufferWidth || mc.getFramebuffer().framebufferHeight != framebufferHeight) {
+                    for (VisualType type : VisualType.getTypes())
+                        type.resize(mc.getFramebuffer());
+                    framebufferWidth = mc.getFramebuffer().framebufferWidth;
+                    framebufferHeight = mc.getFramebuffer().framebufferHeight;
+                }
+                
+                ScaledResolution resolution = new ScaledResolution(mc);
+                int screenWidth = resolution.getScaledWidth();
+                int screenHeight = resolution.getScaledHeight();
+                
+                GlStateManager.pushMatrix();
+                TextureManager manager = mc.getTextureManager();
+                float partialTicks = event.renderTickTime;
+                
+                RenderHelper.enableStandardItemLighting();
+                GlStateManager.disableLighting();
+                GlStateManager.clear(256);
+                GlStateManager.matrixMode(5889);
+                GlStateManager.loadIdentity();
+                GlStateManager.ortho(0.0D, screenWidth, screenHeight, 0.0D, 1000.0D, 3000.0D);
+                GlStateManager.matrixMode(5888);
+                GlStateManager.loadIdentity();
+                GlStateManager.translate(0.0F, 0.0F, -2000.0F);
+                
+                GlStateManager.disableBlend();
+                GlStateManager.disableDepth();
+                GlStateManager.depthMask(false);
+                GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+                GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+                GlStateManager.disableAlpha();
+                GlStateManager.enableBlend();
+                
+                renderVisuals(VisualManager.visuals(VisualCategory.overlay), manager, screenWidth, screenHeight, partialTicks);
+                renderVisuals(VisualManager.visuals(VisualCategory.particle), manager, screenWidth, screenHeight, partialTicks);
+                
+                GlStateManager.matrixMode(5890);
+                GlStateManager.pushMatrix();
+                GlStateManager.loadIdentity();
+                renderVisuals(VisualManager.visuals(VisualCategory.shader), manager, screenWidth, screenHeight, partialTicks);
+                GlStateManager.popMatrix();
+                
+                GlStateManager.depthMask(true);
+                GlStateManager.enableDepth();
+                GlStateManager.enableAlpha();
+                GlStateManager.disableLighting();
+                
+                mc.getFramebuffer().bindFramebuffer(false);
+                GlStateManager.matrixMode(5888);
+                
+                GlStateManager.popMatrix();
+                lastRenderedMessage = null;
+            } else {
+                if (EnhancedVisuals.MESSAGES.enabled) {
+                    if (lastRenderedMessage == null)
+                        lastRenderedMessage = EnhancedVisuals.MESSAGES.pickRandomDeathMessage();
+                    
+                    if (lastRenderedMessage != null)
+                        mc.fontRenderer.drawString("\"" + lastRenderedMessage + "\"", mc.currentScreen.width / 2 - mc.fontRenderer.getStringWidth(lastRenderedMessage) / 2, 114, 16777215);
+                }
+            }
+        }
+    }
+    
+    private static void renderVisuals(Collection<Visual> visuals, TextureManager manager, int screenWidth, int screenHeight, float partialTicks) {
+        if (visuals == null || visuals.isEmpty())
+            return;
+        try {
+            
+            for (Visual visual : visuals) {
+                if (visual.isVisible()) {
+                    GlStateManager.pushMatrix();
+                    visual.render(manager, screenWidth, screenHeight, partialTicks);
+                    GlStateManager.popMatrix();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
