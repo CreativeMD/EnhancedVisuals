cc77fe279eeb9f97a1a35d3ce3aa0284acbc429c
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-01-29 14:34:51 +0100

    Fixed saturation effect not working

--- src/main/java/team/creative/enhancedvisuals/api/type/VisualTypeSaturation.java
+++ src/main/java/team/creative/enhancedvisuals/api/type/VisualTypeSaturation.java
@@ -8,24 +8,24 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import team.creative.enhancedvisuals.api.Visual;
 
 public class VisualTypeSaturation extends VisualTypeShader {
-	
-	public VisualTypeSaturation(String name) {
-		super(name, new ResourceLocation("shaders/post/desaturate.json"));
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void changeProperties(float intensity) {
-		for (Shader mcShader : shaderGroup.getShaders()) {
-			ShaderUniform shaderuniform = mcShader.getShaderManager().getShaderUniform("Saturation");
-			
-			if (shaderuniform != null)
-				shaderuniform.set(intensity);
-		}
-	}
-	
-	@Override
-	public boolean isVisible(Visual visual) {
-		return opacity != 1;
-	}
+    
+    public VisualTypeSaturation(String name) {
+        super(name, new ResourceLocation("shaders/post/desaturate.json"));
+    }
+    
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void changeProperties(float intensity) {
+        for (Shader mcShader : shaderGroup.getShaders()) {
+            ShaderUniform shaderuniform = mcShader.getShaderManager().getShaderUniform("Saturation");
+            
+            if (shaderuniform != null)
+                shaderuniform.set(intensity);
+        }
+    }
+    
+    @Override
+    public boolean isVisible(Visual visual) {
+        return visual.opacity != 1;
+    }
 }
