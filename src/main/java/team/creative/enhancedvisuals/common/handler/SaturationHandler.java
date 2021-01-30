10722172483875356ed979671c4be5d277bfa453
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-01-30 12:02:47 +0100

    Fixed saturation starting at zero

--- src/main/java/team/creative/enhancedvisuals/common/handler/SaturationHandler.java
+++ src/main/java/team/creative/enhancedvisuals/common/handler/SaturationHandler.java
@@ -13,34 +13,35 @@ import team.creative.enhancedvisuals.api.type.VisualTypeSaturation;
 import team.creative.enhancedvisuals.client.VisualManager;
 
 public class SaturationHandler extends VisualHandler {
-	
-	@CreativeConfig
-	public double fadeFactor = 0.0005;
-	
-	@CreativeConfig
-	public DecimalCurve saturation = new DecimalCurve(2, 0, 8, 1);
-	
-	@CreativeConfig
-	public VisualType desaturate = new VisualTypeSaturation("desaturate");
-	
-	public Visual saturationVisual;
-	
-	@Override
-	public void tick(@Nullable EntityPlayer player) {
-		if (saturationVisual == null) {
-			saturationVisual = new Visual(desaturate, 0);
-			VisualManager.add(saturationVisual);
-		}
-		
-		double aimedSaturation = saturation.maxValue;
-		
-		if (player != null)
-			aimedSaturation = saturation.valueAt(player.getFoodStats().getFoodLevel());
-		
-		if (saturationVisual.opacity < aimedSaturation)
-			saturationVisual.opacity = (float) Math.min(saturationVisual.opacity + fadeFactor, aimedSaturation);
-		else if (saturationVisual.opacity > aimedSaturation)
-			saturationVisual.opacity = (float) Math.max(saturationVisual.opacity - fadeFactor, aimedSaturation);
-	}
-	
+    
+    @CreativeConfig
+    public double fadeFactor = 0.0005;
+    
+    @CreativeConfig
+    public DecimalCurve saturation = new DecimalCurve(2, 0, 8, 1);
+    
+    @CreativeConfig
+    public VisualType desaturate = new VisualTypeSaturation("desaturate");
+    
+    public Visual saturationVisual;
+    
+    @Override
+    public void tick(@Nullable EntityPlayer player) {
+        if (saturationVisual == null) {
+            saturationVisual = new Visual(desaturate, 0);
+            saturationVisual.opacity = 1;
+            VisualManager.add(saturationVisual);
+        }
+        
+        double aimedSaturation = saturation.maxValue;
+        
+        if (player != null)
+            aimedSaturation = saturation.valueAt(player.getFoodStats().getFoodLevel());
+        
+        if (saturationVisual.opacity < aimedSaturation)
+            saturationVisual.opacity = (float) Math.min(saturationVisual.opacity + fadeFactor, aimedSaturation);
+        else if (saturationVisual.opacity > aimedSaturation)
+            saturationVisual.opacity = (float) Math.max(saturationVisual.opacity - fadeFactor, aimedSaturation);
+    }
+    
 }
