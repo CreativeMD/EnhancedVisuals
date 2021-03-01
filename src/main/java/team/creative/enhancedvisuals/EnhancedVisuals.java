f91d398b1f407edcefa10645422dae6e03cb023b
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-03-01 17:01:26 +0100

    Added client config gui

--- src/main/java/team/creative/enhancedvisuals/EnhancedVisuals.java
+++ src/main/java/team/creative/enhancedvisuals/EnhancedVisuals.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import team.creative.creativecore.client.CreativeCoreClient;
 import team.creative.creativecore.common.config.holder.ConfigHolderDynamic;
 import team.creative.creativecore.common.config.holder.CreativeConfigRegistry;
 import team.creative.creativecore.common.config.sync.ConfigSynchronization;
@@ -32,43 +33,44 @@ import team.creative.enhancedvisuals.common.visual.VisualRegistry;
 
 @Mod(value = EnhancedVisuals.MODID)
 public class EnhancedVisuals {
-	
-	public static final String MODID = "enhancedvisuals";
-	
-	public static final Logger LOGGER = LogManager.getLogger(EnhancedVisuals.MODID);
-	public static final CreativeNetwork NETWORK = new CreativeNetwork("1.0", LOGGER, new ResourceLocation(EnhancedVisuals.MODID, "main"));
-	public static EVEvents EVENTS;
-	public static DeathMessages MESSAGES;
-	public static EnhancedVisualsConfig CONFIG;
-	
-	public EnhancedVisuals() {
-		DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> FMLJavaModLoadingContext.get().getModEventBus().addListener(this::client));
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
-	}
-	
-	@OnlyIn(value = Dist.CLIENT)
-	private void client(final FMLClientSetupEvent event) {
-		EVClient.init(event);
-	}
-	
-	private void init(final FMLCommonSetupEvent event) {
-		NETWORK.registerType(ExplosionPacket.class);
-		NETWORK.registerType(DamagePacket.class);
-		NETWORK.registerType(PotionPacket.class);
-		
-		MinecraftForge.EVENT_BUS.register(EVENTS = new EVEvents());
-		
-		VisualHandlers.init();
-		MESSAGES = new DeathMessages();
-		if (ModList.get().isLoaded("survive"))
-			SurviveAddon.load();
-		
-		ConfigHolderDynamic root = CreativeConfigRegistry.ROOT.registerFolder(MODID);
-		root.registerValue("general", CONFIG = new EnhancedVisualsConfig(), ConfigSynchronization.CLIENT, false);
-		root.registerValue("messages", MESSAGES);
-		ConfigHolderDynamic handlers = root.registerFolder("handlers", ConfigSynchronization.CLIENT);
-		for (Entry<ResourceLocation, VisualHandler> entry : VisualRegistry.entrySet())
-			handlers.registerValue(entry.getKey().getPath(), entry.getValue());
-	}
-	
+    
+    public static final String MODID = "enhancedvisuals";
+    
+    public static final Logger LOGGER = LogManager.getLogger(EnhancedVisuals.MODID);
+    public static final CreativeNetwork NETWORK = new CreativeNetwork("1.0", LOGGER, new ResourceLocation(EnhancedVisuals.MODID, "main"));
+    public static EVEvents EVENTS;
+    public static DeathMessages MESSAGES;
+    public static EnhancedVisualsConfig CONFIG;
+    
+    public EnhancedVisuals() {
+        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> FMLJavaModLoadingContext.get().getModEventBus().addListener(this::client));
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::init);
+    }
+    
+    @OnlyIn(value = Dist.CLIENT)
+    private void client(final FMLClientSetupEvent event) {
+        EVClient.init(event);
+        CreativeCoreClient.registerClientConfig(MODID);
+    }
+    
+    private void init(final FMLCommonSetupEvent event) {
+        NETWORK.registerType(ExplosionPacket.class);
+        NETWORK.registerType(DamagePacket.class);
+        NETWORK.registerType(PotionPacket.class);
+        
+        MinecraftForge.EVENT_BUS.register(EVENTS = new EVEvents());
+        
+        VisualHandlers.init();
+        MESSAGES = new DeathMessages();
+        if (ModList.get().isLoaded("survive"))
+            SurviveAddon.load();
+        
+        ConfigHolderDynamic root = CreativeConfigRegistry.ROOT.registerFolder(MODID);
+        root.registerValue("general", CONFIG = new EnhancedVisualsConfig(), ConfigSynchronization.CLIENT, false);
+        root.registerValue("messages", MESSAGES);
+        ConfigHolderDynamic handlers = root.registerFolder("handlers", ConfigSynchronization.CLIENT);
+        for (Entry<ResourceLocation, VisualHandler> entry : VisualRegistry.entrySet())
+            handlers.registerValue(entry.getKey().getPath(), entry.getValue());
+    }
+    
 }
--- src/main/java/team/creative/enhancedvisuals/EnhancedVisualsConfig.java
+++ src/main/java/team/creative/enhancedvisuals/EnhancedVisualsConfig.java
@@ -4,16 +4,16 @@ import team.creative.creativecore.common.config.api.CreativeConfig;
 import team.creative.creativecore.common.config.api.ICreativeConfig;
 
 public class EnhancedVisualsConfig implements ICreativeConfig {
-	
-	@CreativeConfig
-	public boolean doEffectsInCreative = false;
-	
-	@CreativeConfig
-	public int waterSubstractFactor = 10;
-	
-	@Override
-	public void configured() {
-		
-	}
-	
+    
+    @CreativeConfig
+    public boolean doEffectsInCreative = false;
+    
+    @CreativeConfig
+    public int waterSubstractFactor = 10;
+    
+    @Override
+    public void configured() {
+        
+    }
+    
 }
