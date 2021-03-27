82191a6646055ad01f2c01c8c2b4128b4612c118
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-03-27 22:50:26 +0100

    Fixed potion visual effects not working

--- src/main/java/team/creative/enhancedvisuals/common/event/EVEvents.java
+++ src/main/java/team/creative/enhancedvisuals/common/event/EVEvents.java
@@ -8,7 +8,6 @@ import com.creativemd.creativecore.common.packet.PacketHandler;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -32,67 +31,67 @@ import team.creative.enhancedvisuals.common.packet.ExplosionPacket;
 import team.creative.enhancedvisuals.common.packet.PotionPacket;
 
 public class EVEvents {
-	
-	private Field size = ReflectionHelper.findField(Explosion.class, new String[] { "field_77280_f", "size" });
-	private Field exploder = ReflectionHelper.findField(Explosion.class, new String[] { "field_77283_e", "exploder" });
-	
-	@SubscribeEvent
-	public void explosion(ExplosionEvent.Detonate event) {
-		if (event.getWorld() != null && !event.getWorld().isRemote) {
-			try {
-				int sourceEntity = -1;
-				Entity source = ((Entity) exploder.get(event.getExplosion()));
-				if (source != null)
-					sourceEntity = source.getEntityId();
-				ExplosionPacket packet = new ExplosionPacket(event.getExplosion().getPosition(), size.getFloat(event.getExplosion()), sourceEntity);
-				for (Entity entity : event.getAffectedEntities())
-					if (entity instanceof EntityPlayerMP)
-						PacketHandler.sendPacketToPlayer(packet, (EntityPlayerMP) entity);
-			} catch (IllegalArgumentException | IllegalAccessException e) {
-				e.printStackTrace();
-			}
-			
-		}
-	}
-	
-	@SubscribeEvent
-	public void impact(ProjectileImpactEvent.Throwable event) {
-		if (!event.getEntity().world.isRemote && event.getEntity() instanceof EntityPotion && !event.isCanceled()) {
-			EntityPotion entity = (EntityPotion) event.getEntity();
-			AxisAlignedBB axisalignedbb = entity.getEntityBoundingBox().grow(4.0D, 2.0D, 4.0D);
-			List<EntityLivingBase> list = entity.world.getEntitiesWithinAABB(EntityLiving.class, axisalignedbb);
-			if (!list.isEmpty()) {
-				for (EntityLivingBase livingentity : list) {
-					if (livingentity.canBeHitWithPotion() && livingentity instanceof EntityPlayer) {
-						double d0 = entity.getDistanceSq(livingentity);
-						if (d0 < 16.0D) {
-							PacketHandler.sendPacketToPlayer(new PotionPacket(Math.sqrt(d0), entity.getPotion()), (EntityPlayerMP) livingentity);
-						}
-					}
-				}
-			}
-		}
-		
-	}
-	
-	@SubscribeEvent
-	public void damage(LivingDamageEvent event) {
-		if (event.getEntity() instanceof EntityPlayerMP)
-			PacketHandler.sendPacketToPlayer(new DamagePacket(event), (EntityPlayerMP) event.getEntity());
-	}
-	
-	@SubscribeEvent
-	@SideOnly(Side.CLIENT)
-	public void clientTick(ClientTickEvent event) {
-		if (event.phase == Phase.START && EVClient.shouldTick()) {
-			EntityPlayer player = Minecraft.getMinecraft().player;
-			VisualManager.onTick(player);
-			
-			SoundMuteHandler.tick();
-		}
-	}
-	
-	public static boolean areEyesInWater(EntityPlayer player) {
-		return player.isInsideOfMaterial(Material.WATER);
-	}
+    
+    private Field size = ReflectionHelper.findField(Explosion.class, new String[] { "field_77280_f", "size" });
+    private Field exploder = ReflectionHelper.findField(Explosion.class, new String[] { "field_77283_e", "exploder" });
+    
+    @SubscribeEvent
+    public void explosion(ExplosionEvent.Detonate event) {
+        if (event.getWorld() != null && !event.getWorld().isRemote) {
+            try {
+                int sourceEntity = -1;
+                Entity source = ((Entity) exploder.get(event.getExplosion()));
+                if (source != null)
+                    sourceEntity = source.getEntityId();
+                ExplosionPacket packet = new ExplosionPacket(event.getExplosion().getPosition(), size.getFloat(event.getExplosion()), sourceEntity);
+                for (Entity entity : event.getAffectedEntities())
+                    if (entity instanceof EntityPlayerMP)
+                        PacketHandler.sendPacketToPlayer(packet, (EntityPlayerMP) entity);
+            } catch (IllegalArgumentException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            
+        }
+    }
+    
+    @SubscribeEvent
+    public void impact(ProjectileImpactEvent.Throwable event) {
+        if (!event.getEntity().world.isRemote && event.getEntity() instanceof EntityPotion && !event.isCanceled()) {
+            EntityPotion entity = (EntityPotion) event.getEntity();
+            AxisAlignedBB axisalignedbb = entity.getEntityBoundingBox().grow(4.0D, 2.0D, 4.0D);
+            List<Entity> list = entity.world.getEntitiesWithinAABBExcludingEntity(entity, axisalignedbb);
+            if (!list.isEmpty()) {
+                for (Entity livingentity : list) {
+                    if (livingentity instanceof EntityLivingBase && ((EntityLivingBase) livingentity).canBeHitWithPotion() && livingentity instanceof EntityPlayer) {
+                        double d0 = entity.getDistanceSq(livingentity);
+                        if (d0 < 16.0D) {
+                            PacketHandler.sendPacketToPlayer(new PotionPacket(Math.sqrt(d0), entity.getPotion()), (EntityPlayerMP) livingentity);
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    @SubscribeEvent
+    public void damage(LivingDamageEvent event) {
+        if (event.getEntity() instanceof EntityPlayerMP)
+            PacketHandler.sendPacketToPlayer(new DamagePacket(event), (EntityPlayerMP) event.getEntity());
+    }
+    
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public void clientTick(ClientTickEvent event) {
+        if (event.phase == Phase.START && EVClient.shouldTick()) {
+            EntityPlayer player = Minecraft.getMinecraft().player;
+            VisualManager.onTick(player);
+            
+            SoundMuteHandler.tick();
+        }
+    }
+    
+    public static boolean areEyesInWater(EntityPlayer player) {
+        return player.isInsideOfMaterial(Material.WATER);
+    }
 }
