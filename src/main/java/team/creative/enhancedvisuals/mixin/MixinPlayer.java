86fc374004113f0166a138e1037876e97f89bddd
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-08-07 12:29:37 +0200

    Fixed crash on server side

--- src/main/java/team/creative/enhancedvisuals/mixin/MixinPlayer.java
+++ src/main/java/team/creative/enhancedvisuals/mixin/MixinPlayer.java
@@ -5,12 +5,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import team.creative.enhancedvisuals.EnhancedVisuals;
 import team.creative.enhancedvisuals.common.handler.VisualHandlers;
 
@@ -28,6 +32,8 @@ public abstract class MixinPlayer extends LivingEntity {
     }
     
     @Inject(at = @At("HEAD"), require = 1, method = "animateHurt(F)V")
+    @OnlyIn(Dist.CLIENT)
+    @Environment(EnvType.CLIENT)
     public void animateHurt(float yaw, CallbackInfo info) {
         if ((Player) (Object) this instanceof LocalPlayer)
             VisualHandlers.DAMAGE.clientHurt();
