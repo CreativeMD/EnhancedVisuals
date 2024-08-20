927b650b5fab50cda8bb49dc24af49358a354193
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-08-20 16:44:01 +0200

    Fixed crash when using enhancedvisuals server side on fabric

--- src/main/java/team/creative/enhancedvisuals/mixin/PlayerMixin.java
+++ src/main/java/team/creative/enhancedvisuals/mixin/PlayerMixin.java
@@ -5,23 +5,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import team.creative.enhancedvisuals.EnhancedVisuals;
-import team.creative.enhancedvisuals.common.handler.VisualHandlers;
 
 @Mixin(Player.class)
-public abstract class MixinPlayer extends LivingEntity {
+public abstract class PlayerMixin extends LivingEntity {
     
-    protected MixinPlayer(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
+    protected PlayerMixin(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
         super(p_20966_, p_20967_);
     }
     
@@ -29,13 +23,5 @@ public abstract class MixinPlayer extends LivingEntity {
             method = "Lnet/minecraft/world/entity/player/Player;actuallyHurt(Lnet/minecraft/world/damagesource/DamageSource;F)V")
     private void actuallyHurt(DamageSource damageSource, float f, CallbackInfo info) {
         EnhancedVisuals.EVENTS.damage((Player) (Object) this, damageSource, f);
-    }
-    
-    @Inject(at = @At("HEAD"), require = 1, method = "animateHurt(F)V")
-    @OnlyIn(Dist.CLIENT)
-    @Environment(EnvType.CLIENT)
-    public void animateHurt(float yaw, CallbackInfo info) {
-        if ((Player) (Object) this instanceof LocalPlayer)
-            VisualHandlers.DAMAGE.clientHurt();
     }
 }
