b8fb8072f39e8ef3b0010708b42fdceb80b4a223
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-08-21 10:48:07 +0200

    Fixed vanilla menu blur rendering causes black artifacts

--- src/main/java/team/creative/enhancedvisuals/mixin/GameRendererMixin.java
+++ src/main/java/team/creative/enhancedvisuals/mixin/GameRendererMixin.java
@@ -4,18 +4,29 @@ import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.shaders.Program;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.BufferUploader;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexFormat;
 import com.mojang.datafixers.util.Pair;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.ShaderInstance;
 import net.minecraft.server.packs.resources.ResourceProvider;
+import team.creative.creativecore.common.util.mc.ColorUtils;
 import team.creative.enhancedvisuals.EnhancedVisuals;
 import team.creative.enhancedvisuals.client.render.EVRenderer;
 
@@ -30,6 +41,42 @@ public class GameRendererMixin {
         } catch (IOException e) {
             EnhancedVisuals.LOGGER.error(e);
         }
+    }
+    
+    @Inject(method = "processBlurEffect(F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/PostChain;process(F)V"), require = 1)
+    public void processBlurEffect(float partialTicks, CallbackInfo info) {
+        Minecraft mc = Minecraft.getInstance();
+        
+        int screenWidth = mc.getWindow().getWidth();
+        int screenHeight = mc.getWindow().getHeight();
+        
+        PoseStack poseStack = new PoseStack();
+        Matrix4f pose = poseStack.last().pose();
+        
+        var shader = RenderSystem.getShader();
+        
+        RenderSystem.disableDepthTest();
+        RenderSystem.depthMask(false);
+        RenderSystem.enableBlend();
+        RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR, GlStateManager.SourceFactor.ONE,
+            GlStateManager.DestFactor.ZERO);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1F);
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        
+        BufferBuilder bufferbuilder = Tesselator.getInstance().begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
+        int color = ColorUtils.BLACK;
+        int z = -90;
+        
+        bufferbuilder.addVertex(pose, screenWidth, screenHeight, z).setColor(color);
+        bufferbuilder.addVertex(pose, screenWidth, 0, z).setColor(color);
+        bufferbuilder.addVertex(pose, 0, 0, z).setColor(color);
+        bufferbuilder.addVertex(pose, 0, screenHeight, z).setColor(color);
+        BufferUploader.drawWithShader(bufferbuilder.buildOrThrow());
+        
+        RenderSystem.disableBlend();
+        RenderSystem.disableDepthTest();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShader(() -> shader);
     }
     
 }
