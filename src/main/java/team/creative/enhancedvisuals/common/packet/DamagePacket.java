0b3eb8b9fdd1bbbaf04a70019d07dbe43e3d2154
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-02-04 15:47:07 +0100

    Reworked how damage source is send to client Fixed spawning blood particles when freezing

--- src/main/java/team/creative/enhancedvisuals/common/packet/DamagePacket.java
+++ src/main/java/team/creative/enhancedvisuals/common/packet/DamagePacket.java
@@ -1,51 +1,47 @@
 package team.creative.enhancedvisuals.common.packet;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.entity.projectile.Arrow;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import team.creative.creativecore.common.network.CanBeNull;
 import team.creative.creativecore.common.network.CreativePacket;
 import team.creative.enhancedvisuals.common.handler.VisualHandlers;
 
 public class DamagePacket extends CreativePacket {
     
-    @CanBeNull
-    public String attackerClass;
-    @CanBeNull
-    public ItemStack stack;
     public float damage;
-    
-    public float distance;
-    public boolean fire;
-    public String source;
+    public ResourceLocation sourceType;
+    public int sourceCauseId;
+    public int sourceDirectId;
+    @CanBeNull
+    public Vec3 sourcePosition;
     
     public DamagePacket(Player target, DamageSource source, float damage) {
         this.damage = damage;
-        Entity attacker = source.getDirectEntity();
-        
-        this.fire = source.is(DamageTypeTags.IS_FIRE);
-        if (attacker instanceof LivingEntity || attacker instanceof Arrow) {
-            attackerClass = attacker.getClass().getName().toLowerCase();
-            this.source = "attacker";
-            
-            if (attacker instanceof LivingEntity && ((LivingEntity) attacker).getMainHandItem() != null)
-                stack = ((LivingEntity) attacker).getMainHandItem();
-            
-        } else
-            this.source = source.getMsgId();
+        this.sourceType = source.typeHolder().unwrapKey().get().location();
+        this.sourceCauseId = source.getEntity() != null ? source.getEntity().getId() : -1;
+        this.sourceDirectId = source.getDirectEntity() != null ? source.getDirectEntity().getId() : -1;
+        this.sourcePosition = source.sourcePositionRaw();
     }
     
     public DamagePacket() {}
     
+    public DamageSource getSource(Level level) {
+        var type = level.registryAccess().lookup(Registries.DAMAGE_TYPE).get().getOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, sourceType));
+        if (this.sourcePosition != null)
+            return new DamageSource(type, this.sourcePosition);
+        return new DamageSource(type, level.getEntity(this.sourceDirectId), level.getEntity(this.sourceCauseId));
+    }
+    
     @Override
     public void executeClient(Player player) {
         if (VisualHandlers.DAMAGE.isEnabled(player))
-            VisualHandlers.DAMAGE.playerDamaged(player, this);
+            VisualHandlers.DAMAGE.playerDamaged(player, getSource(player.level()), damage);
     }
     
     @Override
