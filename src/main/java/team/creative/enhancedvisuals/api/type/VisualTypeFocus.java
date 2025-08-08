ed8b8ce5ed4f5dce29f2bcfceb72d41a66f8971c
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-08-08 15:44:42 +0200

    Fixed not loading correct shader for thirsthandler of toughasnails and survive addon

--- src/main/java/team/creative/enhancedvisuals/api/type/VisualTypeFocus.java
+++ src/main/java/team/creative/enhancedvisuals/api/type/VisualTypeFocus.java
@@ -1,10 +1,11 @@
 package team.creative.enhancedvisuals.api.type;
 
 import net.minecraft.resources.ResourceLocation;
+import team.creative.enhancedvisuals.EnhancedVisuals;
 
 public class VisualTypeFocus extends VisualTypeShader {
     
-    public static final ResourceLocation LOCATION = ResourceLocation.withDefaultNamespace("shaders/post/blobs2.json");
+    public static final ResourceLocation LOCATION = ResourceLocation.tryBuild(EnhancedVisuals.MODID, "shaders/post/blobs2.json");
     
     public VisualTypeFocus(String name) {
         super(name, LOCATION);
